828c945e1d9c0711d9765b5521bbded92718375d
venkateshneu <guri.venkatesh1996@gmail.com>
2024-07-30 03:23:41 -0400

    version 2

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,5 +4,6 @@ import os
 df =pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/master/Ecommerce%20Customers.csv")
 
 df=df.iloc[:,3:]
-df=df[df ["Length of Membership"]>3]
+df=df[df ["Length of Membership"]>1]
+
 df.to_csv(os.path.join("data","customer.csv"))
